f6836b4ee3fb34642e9bf88eb63b69a5b805e3cc
sebsel <sebsel@seblog.nl>
2016-11-23 13:49:36 +0100

    New IndieAuth class
    Still need to use it in the Endpoint class

--- micropub.php
+++ micropub.php
@@ -4,6 +4,7 @@ load(array(
   'sebsel\\micropub\\endpoint'    => __DIR__ . DS . 'lib' . DS . 'endpoint.php',
   'sebsel\\micropub\\interaction' => __DIR__ . DS . 'lib' . DS . 'interaction.php',
   'sebsel\\micropub\\author'      => __DIR__ . DS . 'lib' . DS . 'author.php',
+  'sebsel\\indieauth'             => __DIR__ . DS . 'lib' . DS . 'indieauth.php'
 ));
 
 require(__DIR__ . DS . 'helpers.php');
